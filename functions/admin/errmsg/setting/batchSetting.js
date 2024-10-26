8cc8b42497d8a6348ce76238b6c02acb5da6af67
阿海 <978868928@qq.com>
2024-10-26 15:32:00 +0900

    删除“错误信息”功能页的添加按钮。

--- functions/admin/errmsg/setting/batchSetting.js
+++ functions/admin/errmsg/setting/batchSetting.js
@@ -1,9 +1,4 @@
 export default [{
-    title: "添加",
-    icon: "add",
-    action: "add",
-    field: "id",
-}, {
     title: "删除",
     icon: "delete",
     action: "ajax",
--- functions/admin/errmsg/setting/tableAction.js
+++ functions/admin/errmsg/setting/tableAction.js
@@ -1,9 +1,4 @@
 export default [{
-    title: "编辑",
-    action: "edit",
-    field: "id",
-    url: '../admin/errmsg/init/?do=form',
-}, {
     title: "删除",
     action: "ajax",
     field: "id",
